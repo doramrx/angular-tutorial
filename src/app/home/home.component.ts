82660268ddbb0c5b2df05801edaee6a841341fd4
Isadora <isah3027@gmail.com>
2023-12-26 19:33:35 -0300

    Add the search feature to your app

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,8 +20,21 @@ export class HomeComponent {
 
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
+  filteredLocationList: HousingLocation[] = [];
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
 
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
   }
 }
